461edc217f6ccf362940c94841cb142d349c6984
Nils Schimmelmann <nschimme@users.noreply.github.com>
2026-08-22 15:38:22 -0500

    Update copyright headers and ownership attribution across frontend refactor (#377)

--- frontend/encode_engine.c
+++ frontend/encode_engine.c
@@ -1,7 +1,9 @@
 /*
  * FAAC - Freeware Advanced Audio Coder
  * Copyright (C) 2001 Menno Bakker
- * Copyright (C) 2002-2026 FAAC Team
+ * Copyright (C) 2002-2017 Krzysztof Nikiel
+ * Copyright (C) 2004 Dan Villiom P. Christiansen
+ * Copyright (C) 2026 Nils Schimmelmann
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- frontend/maingui.c
+++ frontend/maingui.c
@@ -1,7 +1,7 @@
 /*
  * FAAC - Freeware Advanced Audio Coder
  * Copyright (C) 2001 Menno Bakker
- * Copyright (C) 2002-2026 FAAC Team
+ * Copyright (C) 2026 Nils Schimmelmann
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
